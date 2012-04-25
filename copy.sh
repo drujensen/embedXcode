3586d00e27edaf3685af10fdd25b0c02965a500e
Dru Jensen <drujensen@gmail.com>
2012-04-24 17:32:06 -0700

    utility to quickly update templates

---
+++ copy.sh
@@ -0,0 +1,4 @@
+rm -r ~/Library/Developer/Xcode/Templates/File\ Templates/embedXcode
+cp -r Templates/File\ Templates/embedXcode ~/Library/Developer/Xcode/Templates/File\ Templates
+rm -r ~/Library/Developer/Xcode/Templates/Project\ Templates/embedXcode
+cp -r Templates/Project\ Template/embedXcode ~/Library/Developer/Xcode/Templates/Project\ Templates
